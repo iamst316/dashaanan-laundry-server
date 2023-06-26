a19af76cdc02577b375bebdf1e9065083f833428
Shubham Tandon <iamshubham316@gmail.com>
2023-06-26 18:01:17 +0530

    chores

--- db/connection.js
+++ db/connection.js
@@ -2,16 +2,14 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 const connection=async()=>{
-    mongoose.set('strictQuery', false);
+  mongoose.set('strictQuery', false);
 
-   try {
-     await mongoose.connect(process.env.MONGO_URL)
-     console.log("mongodb is connected successfully!!!");
-
-   } catch (e) {
+  try {
+    await mongoose.connect(process.env.MONGO_URL)
+    console.log("mongodb is connected successfully!!!");
+  } catch (e) {
     console.log(e);
-
-   }
+  }
 }
 
  module.exports = connection ;
