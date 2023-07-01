d1185211e5a534b295536be1502195d867dbd3a7
Shubham Tandon <iamshubham316@gmail.com>
2023-07-01 15:36:34 +0530

    using jwt in login api

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const {productSchema} = require('./db/productSchema');
 const {stores} = require("./db/stores");
 const {storeSchema} = require("./db/storeSchema");
 const app = express() ;
+require('dotenv').config()
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { userSchema } = require('./db/userSchema');
 connection();
 const Port = 5000;
+const secretkey = process.env.SECRET_KEY;
 app.use(express.json());
 let storeModel = mongoose.model("stores",storeSchema);
 let productModel = mongoose.model("products",productSchema);
@@ -61,17 +64,41 @@ app.post("/register",async(req,res)=>{
         console.log("User Registered")
     }
 })
-
-//--------------------------------------------------
+//----------------------------------------------------
 
 app.post("/login",async(req,res)=>{
     const user = await userModel.find({email:req.body.email})
-    if (user.length==1){
-        console.log("User Exists",user)
 
-    }
-    
+    jwt.sign({user},secretkey,{expiresIn:'1h'},(err,token)=>{
+        console.log("token-->",token)
+        res.json({token})
+    })
 })
+//----------------------------------------------------
+
+app.post("/profile",verifyToken,(req,res)=>{
+    jwt.verify(req.token,secretkey,(err,authData)=>{
+        if (err) res.send("TOKEN INVALID");
+        else{
+            // console.log(req.token);
+            res.send({authData})
+        }
+    })
+})
+
+//---------------------------------------------------
+function verifyToken(req,res,next){
+    const bearerHeader = req.header('Authorization');
+    if(typeof bearerHeader !== undefined){
+        const bearer = bearerHeader.split(" ");
+        const token = bearer[1];
+        req.token = token;
+        next();
+    }
+    else{
+        res.send("Token INVALID");
+    }
+}
 
 //------------------------------------------------
 app.listen(Port,()=>{
